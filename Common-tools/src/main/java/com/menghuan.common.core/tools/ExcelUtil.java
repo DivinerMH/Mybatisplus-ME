21a277a7d8b32bc8713713da3c121ed054f17fac
menghuan <1129094737@qq.com>
2019-12-17 09:14:22 +0800

    tools 12.17 - 09:14

---
+++ Common-tools/src/main/java/com/menghuan.common.core/tools/ExcelUtil.java
@@ -0,0 +1,158 @@
+package com.menghuan.common.core.tools;
+
+import lombok.experimental.UtilityClass;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.xssf.usermodel.extensions.XSSFCellBorder;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTColor;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTColors;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.List;
+
+/**
+ * Excel导出工具
+ * @author bjx
+ * @date 2019-11-28
+ */
+@UtilityClass
+public class ExcelUtil {
+    public void exportExcel(HttpServletResponse response, String fileName, ExcelData data) throws Exception {
+        // 告诉浏览器用什么软件可以打开此文件
+        response.setHeader("content-Type", "application/vnd.ms-excel");
+        // 下载文件的默认名称
+        response.setHeader("Content-Disposition", "attachment;filename="+URLEncoder.encode(fileName + ".xlsx", "utf-8"));
+        exportExcel(data, response.getOutputStream());
+    }
+
+    public void exportExcel(ExcelData data, OutputStream out) throws Exception {
+
+        XSSFWorkbook wb = new XSSFWorkbook();
+        try {
+            String sheetName = data.getName();
+            if (null == sheetName) {
+                sheetName = "Sheet1";
+            }
+            XSSFSheet sheet = wb.createSheet(sheetName);
+            writeExcel(wb, sheet, data);
+
+            wb.write(out);
+        } finally {
+            wb.close();
+        }
+    }
+
+    private void writeExcel(XSSFWorkbook wb, Sheet sheet, ExcelData data) {
+
+        int rowIndex = 0;
+
+        rowIndex = writeTitlesToExcel(wb, sheet, data.getTitles());
+        writeRowsToExcel(wb, sheet, data.getRows(), rowIndex);
+        autoSizeColumns(sheet, data.getTitles().size() + 1);
+
+    }
+
+    private int writeTitlesToExcel(XSSFWorkbook wb, Sheet sheet, List<String> titles) {
+        int rowIndex = 0;
+        int colIndex = 0;
+
+        Font titleFont = wb.createFont();
+        titleFont.setFontName("黑体");
+        titleFont.setBold(true);
+        // titleFont.setFontHeightInPoints((short) 14);
+        titleFont.setColor(IndexedColors.BLACK.index);
+
+        XSSFCellStyle titleStyle = wb.createCellStyle();
+        titleStyle.setAlignment(HorizontalAlignment.CENTER);
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        titleStyle.setFillForegroundColor(IndexedColors.CORNFLOWER_BLUE.getIndex());
+        titleStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        titleStyle.setFont(titleFont);
+
+        /*byte[] bytes = new byte[]{0,0,0};
+        CTColor ctColor = CTColor.Factory.newInstance();
+        ctColor.setRgb(bytes);*/
+//        setBorder(titleStyle, BorderStyle.THIN, XSSFColor.from(ctColor, new DefaultIndexedColorMap()));
+
+        Row titleRow = sheet.createRow(rowIndex);
+        // titleRow.setHeightInPoints(25);
+        colIndex = 0;
+
+        for (String field : titles) {
+            Cell cell = titleRow.createCell(colIndex);
+            cell.setCellValue(field);
+            cell.setCellStyle(titleStyle);
+            colIndex++;
+        }
+
+        rowIndex++;
+        return rowIndex;
+    }
+
+    private int writeRowsToExcel(XSSFWorkbook wb, Sheet sheet, List<List<Object>> rows, int rowIndex) {
+        int colIndex = 0;
+
+        Font dataFont = wb.createFont();
+        dataFont.setFontName("黑体");
+        // dataFont.setFontHeightInPoints((short) 14);
+        dataFont.setColor(IndexedColors.BLACK.index);
+
+        XSSFCellStyle dataStyle = wb.createCellStyle();
+        dataStyle.setAlignment(HorizontalAlignment.CENTER);
+        dataStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        dataStyle.setFont(dataFont);
+
+        /*byte[] bytes = new byte[]{0,0,0};
+        CTColor ctColor = CTColor.Factory.newInstance();
+        ctColor.setRgb(bytes);*/
+//        setBorder(dataStyle, BorderStyle.THIN, XSSFColor.from(ctColor, new DefaultIndexedColorMap()));
+
+        for (List<Object> rowData : rows) {
+            Row dataRow = sheet.createRow(rowIndex);
+            // dataRow.setHeightInPoints(25);
+            colIndex = 0;
+
+            for (Object cellData : rowData) {
+                Cell cell = dataRow.createCell(colIndex);
+                if (cellData != null) {
+                    cell.setCellValue(cellData.toString());
+                } else {
+                    cell.setCellValue("");
+                }
+
+                cell.setCellStyle(dataStyle);
+                colIndex++;
+            }
+            rowIndex++;
+        }
+        return rowIndex;
+    }
+
+    private void autoSizeColumns(Sheet sheet, int columnNumber) {
+
+        for (int i = 0; i < columnNumber; i++) {
+            int orgWidth = sheet.getColumnWidth(i);
+            sheet.autoSizeColumn(i, true);
+            int newWidth = (int) (sheet.getColumnWidth(i) + 100);
+            if (newWidth > orgWidth) {
+                sheet.setColumnWidth(i, newWidth);
+            } else {
+                sheet.setColumnWidth(i, orgWidth);
+            }
+        }
+    }
+
+    private void setBorder(XSSFCellStyle style, BorderStyle border, XSSFColor color) {
+        style.setBorderTop(border);
+        style.setBorderLeft(border);
+        style.setBorderRight(border);
+        style.setBorderBottom(border);
+        style.setBorderColor(XSSFCellBorder.BorderSide.TOP, color);
+        style.setBorderColor(XSSFCellBorder.BorderSide.LEFT, color);
+        style.setBorderColor(XSSFCellBorder.BorderSide.RIGHT, color);
+        style.setBorderColor(XSSFCellBorder.BorderSide.BOTTOM, color);
+    }
+}
---
+++ Common-tools/src/main/java/com/menghuan.common.core/tools/R.java
@@ -0,0 +1,98 @@
+package com.menghuan.common.core.tools;
+
+import com.fitmgr.common.core.constant.CommonConstants;
+import com.fitmgr.common.core.constant.enums.BusinessEnum;
+import com.fitmgr.common.core.constant.enums.ResponseCodeEnum;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * 响应信息主体
+ *
+ * @param <T>
+ * @author dzl
+ */
+@Builder
+@ToString
+@Accessors(chain = true)
+@AllArgsConstructor
+public class R<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    @Setter
+    private int code = CommonConstants.SUCCESS;
+
+    @Getter
+    @Setter
+    private String msg = "success";
+
+
+    @Getter
+    @Setter
+    private T data;
+
+    public R() {
+        super();
+    }
+
+    public R(T data) {
+        super();
+        this.data = data;
+    }
+
+    public R(T data, String msg) {
+        super();
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public R(Throwable e) {
+        super();
+        this.msg = e.getMessage();
+        this.code = CommonConstants.FAIL;
+    }
+
+
+    public static <T> R<T> ok() {
+        return restResult(null, ResponseCodeEnum.SUCCESS.getCode(), ResponseCodeEnum.SUCCESS.getDesc());
+    }
+
+    public static <T> R<T> ok(T data) {
+        return restResult(data, ResponseCodeEnum.SUCCESS.getCode(), ResponseCodeEnum.SUCCESS.getDesc());
+    }
+
+    public static <T> R<T> ok(T data, String msg) {
+        return restResult(data, ResponseCodeEnum.SUCCESS.getCode(), msg);
+    }
+
+    public static <T> R<T> failed() {
+        return restResult(null, ResponseCodeEnum.ERROR.getCode(), ResponseCodeEnum.ERROR.getDesc());
+    }
+
+    public static <T> R<T> failed(String msg) {
+        return restResult(null, ResponseCodeEnum.ERROR.getCode(), msg);
+    }
+
+    public static <T> R<T> failed(T data) {
+        return restResult(data, ResponseCodeEnum.ERROR.getCode(), ResponseCodeEnum.ERROR.getDesc());
+    }
+
+    public static <T> R<T> failed(T data, String msg) {
+        return restResult(data, ResponseCodeEnum.ERROR.getCode(), msg);
+    }
+
+    public static <T> R<T> failed(BusinessEnum businessEnum) {
+        return restResult(null, businessEnum.getCode(), businessEnum.getDescription());
+    }
+
+    private static <T> R<T> restResult(T data, int code, String msg) {
+        R<T> apiResult = new R<>();
+        apiResult.setCode(code);
+        apiResult.setData(data);
+        apiResult.setMsg(msg);
+        return apiResult;
+    }
+}
