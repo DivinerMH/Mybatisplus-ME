14be220d85f1ad628883c60615fbe271ea553e46
menghuan <1129094737@qq.com>
2019-12-25 15:08:14 +0800

    tools 12.25- 15:07 .gitignore 配置完善5

--- mybatisplus/src/main/java/com/fitmgr/quota/QuotaApplication.java
+++ mybatisplus/src/main/java/com/fitmgr/quota/QuotaApplication.java
@@ -1,21 +1,24 @@
-package com.menghuan.admin;
+
+
+package com.fitmgr.quota;
+
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 /**
  * @author menghuan
  * @date 2018年06月21日
- * 登录权限系统
+ * 配额管理系统
  */
+
 @SpringBootApplication
-@MapperScan(value = "com.menghuan.admin.mapper")
-
-public class AdminplusApplication {
-
+@MapperScan(value = "com.fitmgr.admin.mapper")
+public class QuotaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AdminplusApplication.class, args);
+        SpringApplication.run(QuotaApplication.class, args);
     }
 
 }
