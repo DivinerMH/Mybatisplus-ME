8fb2f2451d20e243831797ae18a1f1013ca6954d
menghuan <1129094737@qq.com>
2019-12-25 14:54:13 +0800

    tools 12.25- 15:07 .gitignore 配置完善4

---
+++ common-tools-me/src/main/java/com/menghuan.common.core/tools/JsonResultVO.java
@@ -0,0 +1,36 @@
+package com.menghuan.common.core.tools;
+
+import lombok.Data;
+
+@Data
+public class JsonResultVO {
+
+    private int code;       //结果状态码
+    private String msg;     //结果信息
+    private Object data;
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
