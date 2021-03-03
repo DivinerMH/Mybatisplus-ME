56c693e1b8d911e50abb4895a910514288f71502
menghuan <1129094737@qq.com>
2021-03-03 17:57:15 +0800

    21.03.02 - 及时测试

--- common-tools-me/src/test/java/com/hanchan/codeTest_2021_02/Lazy/MyLazyTest.java
+++ common-tools-me/src/test/java/com/hanchan/codeTest_2021_02/Lazy/MyLazyTest.java
@@ -2,9 +2,7 @@ package com.hanchan.codeTest_2021_02.Lazy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
