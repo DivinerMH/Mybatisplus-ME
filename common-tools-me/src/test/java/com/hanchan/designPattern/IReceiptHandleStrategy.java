adfaf670a609735021b1c56736a542e1ca3286ee
menghuan <1129094737@qq.com>
2021-05-07 10:43:29 +0800

    21.05.07 设计模式测试

---
+++ common-tools-me/src/test/java/com/hanchan/designPattern/IReceiptHandleStrategy.java
@@ -0,0 +1,12 @@
+package com.hanchan.designPattern;
+
+/**
+ * 回执处理策略接口
+ * @Author: menghuan
+ * @Date: 2021/5/7 10:41
+ */
+public interface IReceiptHandleStrategy {
+
+    void handleReceipt(Receipt receipt);
+
+}
