f1554d1430b205c003ace99fbf7dc8d3ae1330f0
menghuan <1129094737@qq.com>
2021-06-30 14:45:10 +0800

    21.06.30 类型转换

---
+++ mybatisplus/src/main/java/com/linksame/iot/office/GeneratorApplication.java
@@ -0,0 +1,18 @@
+package com.linksame.iot.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @Author: menghuan
+ * @Date: 2021/6/24 14:54
+ */
+
+@SpringCloudApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+
+}
