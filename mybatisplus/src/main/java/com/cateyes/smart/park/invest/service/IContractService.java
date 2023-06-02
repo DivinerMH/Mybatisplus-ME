44c199be55c7ab542f04710f3a635aab22802b84
menghuan <1096464960@qq.com>
2023-06-02 19:32:47 +0800

    init 3

--- mybatisplus/src/main/java/com/cateyes/smart/park/invest/service/IContractService.java
+++
@@ -1,9 +0,0 @@
-package com.cateyes.smart.park.invest.service;
-
-/**
- * 合同基本信息表(SpContract)service
- *
- * @author CatEyes
- * @since 2023-05-08 10:07:41
- */
-public interface IContractService extends SpBaseService<Contract, ReqContractDto> {}
