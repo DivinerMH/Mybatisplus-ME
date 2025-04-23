ba476790e92592858409e648e746b0ec34d8c7e5
menghuan <diviner119@163.com>
2025-04-23 10:24:51 +0800

    250422 - 节假日构建 - 优化 2

---
+++ DM-DDL/src/main/java/com/menghuan/ddl/DdlApplication.java
@@ -0,0 +1,13 @@
+package com.menghuan.ddl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdlApplication.class, args);
+    }
+
+}
