4297dfd1043959fc12b94618e7774a56436750b1
menghuan <diviner119@163.com>
2025-09-09 16:37:39 +0800

    250909 - 参数校验 - 优化

--- HuanTools/src/main/java/com/menghuan/HuanTools/valid/BusinessValidator.java
+++ HuanTools/src/main/java/com/menghuan/HuanTools/valid/BusinessValidator.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import com.menghuan.HuanTools.bean.api.ApiCode;
 import com.menghuan.HuanTools.exception.ServerException;
 
-
 /**
  * 通用业务校验工具类
  * 
@@ -17,66 +16,82 @@ import com.menghuan.HuanTools.exception.ServerException;
 public class BusinessValidator<E extends RuntimeException> {
 
     /**
-     * 是有构造,防止被实现
+     * 私有构造 - 防止被实现重写
      */
     private BusinessValidator() {
         super();
     }
 
-    public static void isBlank(String value) {
-        if (StringUtils.isNotBlank(value)) {
+    /**
+     * 校验：参数字符串为空（否则抛异常）
+     * 
+     * @param str 参数字符串
+     */
+    public static void isBlank(String str) {
+        if (StringUtils.isNotBlank(str)) {
             throw new ServerException("参数不为空");
         }
     }
 
-    public static void isBlank(String value, ApiCode apiCode) {
-        if (StringUtils.isNotBlank(value)) {
+    /**
+     * 校验：参数字符串为空（否则抛异常）
+     * 
+     * @param str 参数字符串
+     * @param apiCode REST API 响应码
+     */
+    public static void isBlank(String str, ApiCode apiCode) {
+        if (StringUtils.isNotBlank(str)) {
             throw new ServerException(apiCode);
         }
     }
 
     /**
-     * 校验字符串必须为空（否则抛异常）
+     * 校验：参数字符串为空（否则抛异常）
      * 
-     * @param value 被校验字符串
-     * @param msg 参数名称（用于错误消息）
+     * @param str 参数字符串
+     * @param attributeName 参数属性名（用于错误消息）
      */
-    public static void isBlank(String value, String msg) {
-        if (StringUtils.isNotBlank(value)) {
-            throw new ServerException(String.format("参数{%s}不为空", msg));
+    public static void isBlank(String str, String attributeName) {
+        if (StringUtils.isNotBlank(str)) {
+            throw new ServerException(String.format("参数{%s}不为空", attributeName));
         }
     }
 
-    public static void isNotBlank(String value) {
-        if (StringUtils.isBlank(value)) {
+    public static void isNotBlank(String str) {
+        if (StringUtils.isBlank(str)) {
             throw new ServerException("参数为空");
         }
     }
 
-    public static void isNotBlank(String value, ApiCode apiCode) {
-        if (StringUtils.isBlank(value)) {
+    public static void isNotBlank(String str, ApiCode apiCode) {
+        if (StringUtils.isBlank(str)) {
             throw new ServerException(apiCode);
         }
     }
 
-    /**
-     * 校验字符串必须非空（否则抛异常）
-     * 
-     * @param value 被校验字符串
-     * @param msg 参数名称（用于错误消息）
-     */
-    public static void isNotBlank(String value, String msg) {
-        if (StringUtils.isBlank(value)) {
-            throw new ServerException(String.format("参数{%s}为空", msg));
+    public static void isNotBlank(String str, String attributeName) {
+        if (StringUtils.isBlank(str)) {
+            throw new ServerException(String.format("参数{%s}为空", attributeName));
         }
     }
 
+    /**
+     * 校验：参数对象为 NULL（否则抛异常）
+     * 
+     * @param obj 参数对象
+     */
     public static void isNull(Object obj) {
         if (obj != null) {
             throw new ServerException("参数不为NULL");
         }
     }
 
+    /**
+     * 校验：参数对象为 NULL（否则抛异常）
+     * 
+     * @param obj 参数对象
+     * @param apiCode REST API 响应码
+     */
     public static void isNull(Object obj, ApiCode apiCode) {
         if (obj != null) {
             throw new ServerException(apiCode);
@@ -84,14 +99,14 @@ public class BusinessValidator<E extends RuntimeException> {
     }
 
     /**
-     * 校验对象必须为null（否则抛异常）
+     * 校验：参数对象为 NULL（否则抛异常）
      * 
-     * @param obj 被校验对象
-     * @param msg 参数名称（用于错误消息）
+     * @param obj 参数对象
+     * @param objName 参数对象名（用于错误消息）
      */
-    public static void isNull(Object obj, String msg) {
+    public static void isNull(Object obj, String objName) {
         if (obj != null) {
-            throw new ServerException(String.format("参数{%s}不为NULL", msg));
+            throw new ServerException(String.format("参数{%s}不为NULL", objName));
         }
     }
 
@@ -107,12 +122,6 @@ public class BusinessValidator<E extends RuntimeException> {
         }
     }
 
-    /**
-     * 校验对象必须为null（否则抛异常）
-     * 
-     * @param obj 被校验对象
-     * @param msg 参数名称（用于错误消息）
-     */
     public static void isNotNull(Object obj, String msg) {
         if (null == obj) {
             throw new ServerException(String.format("参数{%s}为NULL", msg));
@@ -120,10 +129,10 @@ public class BusinessValidator<E extends RuntimeException> {
     }
 
     /**
-     * 校验集合必须为空（否则抛异常）
+     * 校验：集合为空（否则抛异常）
      * 
-     * @param collection 被校验集合
-     * @param paramName 参数名称（用于错误消息）
+     * @param collection 集合
+     * @param paramName 参数集合名称（用于错误消息）
      */
     public static void isEmpty(Collection<?> collection, String paramName) {
         if (collection != null && !collection.isEmpty()) {
@@ -132,10 +141,10 @@ public class BusinessValidator<E extends RuntimeException> {
     }
 
     /**
-     * 校验集合必须非空（否则抛异常）
+     * 校验：集合非空（否则抛异常）
      * 
-     * @param collection 被校验集合
-     * @param paramName 参数名称（用于错误消息）
+     * @param collection 集合
+     * @param paramName 参数集合名称（用于错误消息）
      */
     public static void isNotEmpty(Collection<?> collection, String paramName) {
         if (collection == null || collection.isEmpty()) {
@@ -143,12 +152,24 @@ public class BusinessValidator<E extends RuntimeException> {
         }
     }
 
-    public static void isTrue(boolean condition, String str) {
+    /**
+     * 校验：condition 为 True（否则抛异常）
+     * 
+     * @param condition 判断条件
+     * @param msg 信息（异常信息）
+     */
+    public static void isTrue(boolean condition, String msg) {
         if (!condition) {
-            throw new ServerException(str);
+            throw new ServerException(msg);
         }
     }
 
+    /**
+     * 校验：condition 为 True（否则抛异常）
+     * 
+     * @param condition 判断条件
+     * @param apiCode REST API 响应码
+     */
     public static void isTrue(boolean condition, ApiCode apiCode) {
         if (!condition) {
             throw new ServerException(apiCode);
@@ -156,16 +177,16 @@ public class BusinessValidator<E extends RuntimeException> {
     }
 
     /**
-     * 通用条件校验（否则抛异常）
+     * 校验：condition 为 True（否则抛异常）
      * 
-     * @param condition 必须为true的条件
-     * @param apiCode 错误码
-     * @param message 错误描述（支持%s占位符）
+     * @param condition 判断条件
+     * @param apiCode REST API 响应码
+     * @param msg 信息（异常信息）
      * @param params 占位符参数
      */
-    public static void isTrue(boolean condition, ApiCode apiCode, String message, Object... params) {
+    public static void isTrue(boolean condition, ApiCode apiCode, String msg, Object... params) {
         if (!condition) {
-            throw new ServerException(apiCode, String.format(message, params));
+            throw new ServerException(apiCode, String.format(msg, params));
         }
     }
 
